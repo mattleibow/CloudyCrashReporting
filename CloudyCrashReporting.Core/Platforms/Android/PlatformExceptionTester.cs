e772b9c66cfef0cd67f941783015af3f177e6183
Matthew Leibowitz <mattleibow@live.com>
2024-01-08 11:50:42 +0200

    Renamed

--- CloudyCrashReporting.Core/Platforms/Android/PlatformExceptionTester.cs
+++ CloudyCrashReporting.Core/Platforms/Android/PlatformExceptionTester.cs
@@ -7,6 +7,8 @@ public partial class PlatformExceptionTester
 {
     public partial void QueueInvalidOperation()
     {
+        // TODO: this is not correct as the types are managed types
+        
         var h = new Handler(Looper.MainLooper);
         var r = new Runnable(() => { });
         h.PostDelayed(r, 1 * 1000);
